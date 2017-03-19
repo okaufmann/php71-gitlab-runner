af262a8fba3c702f2d02ead4bd3deeb8acd84221
Oliver Kaufmann <okaufmann91@gmail.com>
2017-03-19 19:50:13 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
+
+# Install mysql driver
+# Here you can install any other extension that you need
+docker-php-ext-install mbstring mcrypt pdo_mysql curl json intl gd xml zip bz2 opcache
+
+# Install phpunit, the tool that we will use for testing
+curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+chmod +x /usr/local/bin/phpunit
+
+# Install Composer Package manager
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
+# Install global composer packages
+composer global require "hirak/prestissimo:^0.3"
