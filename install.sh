bbbdd46eff7838789469aa0e12b692724f83672b
Oliver Kaufmann <okaufmann91@gmail.com>
2017-03-21 17:21:24 +0100

    Install bzip2

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
+apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq bzip2
 
 # Install mysql driver
 # Here you can install any other extension that you need
